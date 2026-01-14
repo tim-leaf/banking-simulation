a1951d46cd5ecce913341870f507b755015be6ea
Tim Leaf <leflemtim33@gmail.com>
2026-01-14 03:51:35 +0100

    Initial Commit

---
+++ SQL Banking Simulation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SQL Banking Simulation
+//
+//  Created by Tim Le Flem on 14/01/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return EXIT_SUCCESS;
+}
