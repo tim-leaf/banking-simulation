c177af9780d02bebc7bb1e9080e41cbe6a2597de
Tim Le Flem <timleflem@mac.home>
2026-02-08 18:04:23 +0100

    added CI build for Window / Linux

---
+++ build/CMakeFiles/BankingSimulation.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/timleflem/Code/C++/GitHub Projects/Banking-Simulation/src/account.cpp" "CMakeFiles/BankingSimulation.dir/src/account.cpp.o" "gcc" "CMakeFiles/BankingSimulation.dir/src/account.cpp.o.d"
+  "/Users/timleflem/Code/C++/GitHub Projects/Banking-Simulation/src/bank.cpp" "CMakeFiles/BankingSimulation.dir/src/bank.cpp.o" "gcc" "CMakeFiles/BankingSimulation.dir/src/bank.cpp.o.d"
+  "/Users/timleflem/Code/C++/GitHub Projects/Banking-Simulation/src/customer.cpp" "CMakeFiles/BankingSimulation.dir/src/customer.cpp.o" "gcc" "CMakeFiles/BankingSimulation.dir/src/customer.cpp.o.d"
+  "/Users/timleflem/Code/C++/GitHub Projects/Banking-Simulation/src/main.cpp" "CMakeFiles/BankingSimulation.dir/src/main.cpp.o" "gcc" "CMakeFiles/BankingSimulation.dir/src/main.cpp.o.d"
+  "/Users/timleflem/Code/C++/GitHub Projects/Banking-Simulation/src/transaction.cpp" "CMakeFiles/BankingSimulation.dir/src/transaction.cpp.o" "gcc" "CMakeFiles/BankingSimulation.dir/src/transaction.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
